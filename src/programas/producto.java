4ce149996d19230bfa8e808f301abfe284e5cc2c
Cristian <Cristian@DESKTOP-P759R2J>
2025-06-09 21:59:13 -0500

    carga proyecto 4

--- src/programas/producto.java
+++ src/programas/producto.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class producto {
     public static void main(String[]args){
         //declarar variables
-        int n1,n2,n3,producto,suma;
+        int n1,n2,n3,producto,suma,suma2;
         Scanner lectura=new Scanner(System.in);
         //entrada
         System.out.print("Ingresar número 1:");
@@ -17,9 +17,12 @@ public class producto {
         //proceso
         producto=n1*n2*n3;
         suma=n1+n2+n3;
+        suma2=n1+n2;
         //salida
         System.out.println("El producto de los números es:"+producto );
         System.out.println("La suma de los números es:"+suma);
+        //SE PROCEDIO CON LA MODIFICACION
+        System.out.println("Suma de dos primeras compras es:"+suma2);
     }
     
 }
